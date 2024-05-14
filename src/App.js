15570fbca541239d65b3d694fe0f210435b14519
Aya Ahmed <ayaahmeed963@gmail.com>
2024-05-14 13:24:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import './App.css';
-import Buttons from './Buttons';
-import Header from './Header';
-import Posts from './Posts';
+import HW from './Hw';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ display: "flex", width: "60%" }}>
-          <div style={{ width: "70%" }}>
-            <Posts />
-            <Posts />
-            <Posts />
-            <Posts />
-            <Posts />
-          </div>
-          <div style={{ width: "30%" }}>
-            <Buttons />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+    <HW/>
+    </>
   );
 }
-
 
 export default App;
--- src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-    return (
-        <div className={"myHeader"}>
-            <h1 className={"headerText"}>Noor web page</h1>
-        </div>
-    )
-}
